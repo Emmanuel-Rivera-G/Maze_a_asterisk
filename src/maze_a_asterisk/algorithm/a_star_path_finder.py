699bcbdc3f7f84d81b4818849c5cc809be1575fc
Emmanuel-Rivera-G <U22222056@utp.edu.pe>
2025-06-24 13:27:01 -0500

    Mejorando nombres

--- src/maze_a_asterisk/algorithm/a_star_path_finder.py
+++ src/maze_a_asterisk/algorithm/a_star_path_finder.py
@@ -1,8 +1,7 @@
 import math
 
-
 def euclidean(a, b) -> float:
-    return math.hypot(a[0] - b[0], a[1] - b[1])
+    return math.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
 
 
 def _min_puntaje_node(cola):
@@ -39,26 +38,26 @@ class AStarPathFinder:
                     yield nx, ny
 
     def find_path(self):
-        cola_abierta = [(0.0, self.start)]
+        cola_abierta = [(0.0001, self.start)]
 
-        padre: dict = {}
+        visitados: dict = {}
         costo_g: dict = {self.start: 0.0}
         costo_f: dict = {self.start: euclidean(self.start, self.end)}
 
         while cola_abierta:
-            f_actual, actual = _min_puntaje_node(cola_abierta)
+            puntaje_f_actual, actual = _min_puntaje_node(cola_abierta)
 
             if actual == self.end:
                 camino = [actual]
-                while actual in padre:
-                    actual = padre[actual]
+                while actual in visitados:
+                    actual = visitados[actual]
                     camino.append(actual)
                 return list(reversed(camino))
 
             for vecino in self._neighbors(actual):
                 costo_temporal = costo_g[actual] + 1.0
                 if costo_temporal < costo_g.get(vecino, math.inf):
-                    padre[vecino] = actual
+                    visitados[vecino] = actual
                     costo_g[vecino] = costo_temporal
                     costo_f[vecino] = costo_temporal + euclidean(vecino, self.end)
 
